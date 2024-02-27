7a39e459153c7cde50264ba4482cb8a6fe916f60
nic-barbara <nbar5346@uni.sydney.edu.au>
2024-02-27 12:32:43 +1100

    Added extra formatting for python plots, including better colours

--- python/format_figures.py
+++ python/format_figures.py
@@ -13,11 +13,11 @@ Author: Jack Naylor
 Contact: jack.naylor@sydney.edu.au
 """
 
-
+from cycler import cycler
 import matplotlib.pyplot as plt
 
 
-def startup_plotting(font_size=16,line_width=1.5,output_dpi=300,tex_backend=False):
+def startup_plotting(font_size=14,line_width=1.5,output_dpi=600,tex_backend=False):
 
     if tex_backend:
         try:
@@ -31,27 +31,34 @@ def startup_plotting(font_size=16,line_width=1.5,output_dpi=300,tex_backend=Fals
             plt.rcParams.update({"font.family": "serif",
                     "font.serif": ["Computer Modern Roman"],
                         })
-    
-    # TODO: Colour-scheme.
 
-    # Turn on axes grids.
-    plt.rcParams.update({"axes.grid" : True, 
-                        "legend.framealpha": 1,
-                        "legend.edgecolor": [1,1,1],
-                        "lines.linewidth": line_width,
-                        "savefig.dpi": output_dpi,
-                        "savefig.format": 'pdf'})
+    # Format lines, axes, grids, etc.
+    # Colour map from https://www.nature.com/articles/nmeth.1618?WT.ec_id=NMETH-201106
+    # Re-ordered as per https://docs.makie.org/stable/explanations/colors/#colormaps
+    plt.rcParams.update({
+        "lines.linewidth": line_width,
+        
+        "axes.grid" : True, 
+        "axes.grid.which": "major",
+        "axes.linewidth": 0.5,
+        "axes.prop_cycle": cycler("color", [
+            "#0072B2", "#E69F00", "#009E73", "#CC79A7", 
+            "#56B4E9", "#D55E00", "#F0E442", "#000000"]),
+
+        "grid.linewidth": 0.25,
+        "grid.alpha": 0.5,
+        
+        "legend.framealpha": 0.7,
+        "legend.edgecolor": [1,1,1],
+        
+        "savefig.dpi": output_dpi,
+        "savefig.format": 'pdf'
+    })
 
     # Change default font sizes.
     plt.rc('font', size=font_size) #controls default text size
     plt.rc('axes', titlesize=font_size) #fontsize of the title
     plt.rc('axes', labelsize=font_size) #fontsize of the x and y labels
-    plt.rc('xtick', labelsize=font_size) #fontsize of the x tick labels
-    plt.rc('ytick', labelsize=font_size) #fontsize of the y tick labels
+    plt.rc('xtick', labelsize=0.8*font_size) #fontsize of the x tick labels
+    plt.rc('ytick', labelsize=0.8*font_size) #fontsize of the y tick labels
     plt.rc('legend', fontsize=0.85*font_size) #fontsize of the legend 
-
-
-
-
-
-
